f9ab53f44d7071b1dfd62bb1728bb794793359dd
Igor-Yarakhovich <igtopor@yandex.ru>
2022-02-21 19:11:33 +0300

    nav scroll

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
 import style from './Nav.module.scss'
+import {Link} from "react-scroll";
 
 export const Nav: React.FC = () => {
     return (
         <div className={style.container}>
             <div className={style.nav}>
-                <a className={style.button} href={'#homeId'}>HOME</a>
-                <a className={style.button} href={'#aboutId'}>ABOUT US</a>
-                <a className={style.button} href={'#skillsId'}>SKILLS</a>
-                <a className={style.button} href={'#portfolioId'}>PORTFOLIO</a>
-                <a className={style.button} href={'#contactId'}>CONTACTS</a>
+                <Link activeClass={style.active}
+                      className={style.button}
+                      to="homeId"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={700}
+                >HOME</Link>
+
+                <Link activeClass={style.active}
+                      className={style.button}
+                      to="aboutId"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={700}
+                >ABOUT US</Link>
+
+                <Link activeClass={style.active}
+                      className={style.button}
+                      to="skillsId"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={700}
+                >SKILLS</Link>
+
+                <Link activeClass={style.active}
+                      className={style.button}
+                      to="portfolioId"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={700}
+                >PORTFOLIO</Link>
+
+                <Link activeClass={style.active}
+                      className={style.button}
+                      to="contactId"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={700}
+                >CONTACTS</Link>
             </div>
         </div>
     )
