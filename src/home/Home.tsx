0564098135c0ff0600796a2d2e4ef271f5bc1c0c
Igor-Yarakhovich <igtopor@yandex.ru>
2022-02-21 19:42:09 +0300

    new commit

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import style from './Home.module.scss'
 import container from './../common/styles/container.module.css'
-import {Button} from "../common/components/button/button";
 import ReactTypingEffect from "react-typing-effect";
+import {Link} from "react-scroll";
 
 export const Home: React.FC = () => {
     return (
@@ -13,10 +13,17 @@ export const Home: React.FC = () => {
                         Hello, <br/>
                         I'm Igor Yarakhovich a <br/>
                         <ReactTypingEffect
-                            text={['Front-end Developer','Front-end Engineer']}
-                            />
+                            text={['Front-end Developer', 'Front-end Engineer']}
+                        />
                     </div>
-                    <Button name={'See Who I\'m'}/>
+                    <Link to="aboutId"
+                          spy={true}
+                          smooth={true}
+                          offset={-71}
+                          duration={700}
+                    >
+                        <button className={style.button}>See Who I'm</button>
+                    </Link>
                 </div>
             </div>
         </div>
