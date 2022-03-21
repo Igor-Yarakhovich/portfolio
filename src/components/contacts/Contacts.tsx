128b3ab05712bfdf4e8d43d6699e210e80c249a3
Igor-Yarakhovich <igtopor@yandex.ru>
2022-03-21 13:36:53 +0300

    add eslint

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import style from './Contacts.module.scss'
-
+import axios from 'axios'
 import container from 'common/styles/container.module.css'
 import { Title } from 'common/components/title/Title'
+import { Formik, Field, Form } from 'formik'
 
 export const Contacts: React.FC = () => {
     return (
@@ -11,25 +12,53 @@ export const Contacts: React.FC = () => {
                 className={`${container.container} ${style.containerContacts}`}
             >
                 <Title name="contacts" />
-                <form className={style.formContacts}>
-                    <input
-                        type="text"
-                        placeholder="Name"
-                        className={style.input}
-                    />
-                    <input
-                        type="text"
-                        placeholder="e-mail"
-                        className={style.input}
-                    />
-                    <textarea
-                        placeholder="Message"
-                        className={style.textarea}
-                    />
-                    <button type="submit" className={style.button}>
-                        Send message
-                    </button>
-                </form>
+                <div className={style.info}>Fill out the form to hire me :</div>
+                <Formik
+                    initialValues={{
+                        name: '',
+                        email: '',
+                        message: '',
+                    }}
+                    onSubmit={(values) => {
+                        axios
+                            .post(
+                                'https://smtp-server-email-portfolio.herokuapp.com/sendMessage',
+                                {
+                                    values,
+                                }
+                            )
+                            .then(() => {
+                                alert('Send message')
+                            })
+                    }}
+                >
+                    <Form className={style.formContacts}>
+                        <Field
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder="Your name"
+                            className={style.input}
+                        />
+                        <Field
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="Email"
+                            className={style.input}
+                        />
+                        <Field
+                            id="message"
+                            name="message"
+                            placeholder="Message"
+                            type="text"
+                            className={style.textarea}
+                        />
+                        <button type="submit" className={style.button}>
+                            Send message
+                        </button>
+                    </Form>
+                </Formik>
             </div>
         </div>
     )
